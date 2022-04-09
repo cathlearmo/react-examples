78a984e25a857d3314fa513acbddf7f646ca2453
Catherine Learmonth <cathlearmo@gmail.com>
2022-04-09 15:41:25 +0100

    update test for App.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders side nav links', () => {
   render(<App />);
   const fetchDemoLink = screen.getByText(/Fetch demo/i);
   expect(fetchDemoLink).toBeInTheDocument();
+  const tableDemoLink = screen.getByText(/Table demo/i);
+  expect(tableDemoLink).toBeInTheDocument();
 });
+
