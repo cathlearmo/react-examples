76fee2e1bd194c21789faf4ea011eef5c492a849
Catherine Learmonth <cathlearmo@gmail.com>
2022-04-10 21:02:37 +0100

    add a welcome banner

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import {
   Routes, Route, Link } from "react-router-dom";
 import { PersonTable } from './table/tableType.jsx';
 import { SearchPage } from './search/searchPage.jsx';
+import { Banner } from './banners/banner.jsx';
 import './App.css';
+
+const WelcomePage = () => {
+  const textOne = "Welcome to my react demo web site.";
+  const textTwo = "Click on a link in the side nav to get started.";
+  return <Banner text1={textOne} text2={textTwo}/>
+}
 
 /*use React Router for client-side page navigation*/
 
@@ -28,6 +35,7 @@ function App() {
               renders the first one that matches the current URL. */}
           <div className={"main-content"}>
             <Routes>
+              <Route path="/" element={<WelcomePage />}/>
               <Route path="/fetch" element={<SearchPage />}/>
               <Route path="/table" element={<PersonTable />}/>
             </Routes>
