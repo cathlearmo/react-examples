d89af66f98ef4a2de356a1a31e6fd28c0f02e22c
Catherine Learmonth <cathlearmo@gmail.com>
2022-04-10 19:46:15 +0100

    use banner in table page

--- src/table/tableType.jsx
+++ src/table/tableType.jsx
@@ -5,16 +5,19 @@ import inputData from '../data/mockData.json';
 import { PageSize } from "../pagination/paginationConstants.js";
 import { useCurrentPage } from "../customHooks/useCurrentPage.jsx";
 import { useData } from "../customHooks/useData.jsx";
+import { Banner } from '../banners/banner.jsx';
 import './table.scss';
 
 export function PersonTable() {
     const { data, setData} = useData(inputData);
     const { currentPage, setCurrentPage, currentData } = useCurrentPage(data);
 
+    const textOne = "This page uses the same pagination component as the fetch demo page.";
+    const textTwo = "Click on a table header to sort the data by the column value.";
+
     return (
         <div className="page-right">
-            <p>This page uses the same pagination component as the fetch example page</p>
-            <p>Click on a table header to sort the data by the column value</p>
+            <Banner text1={textOne} text2={textTwo}/>
             <Table 
                 className="table-class" 
                 tableData={currentData} 
