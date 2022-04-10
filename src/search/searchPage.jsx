7337d7046ebd1cd391b7ba2a0da069198ed3c9b2
Catherine Learmonth <cathlearmo@gmail.com>
2022-04-10 20:15:16 +0100

    update search page text

--- src/search/searchPage.jsx
+++ src/search/searchPage.jsx
@@ -4,8 +4,8 @@ import { SearchQuery } from '../search/search.jsx';
 
 export function SearchPage() {
 
-    const textOne = "The search page uses a Sky TV web service.";
-    const textTwo = "You will need to enter a sky TV related search query to see results.";
+    const textOne = "The fetch demo uses a Sky TV web service.";
+    const textTwo = "You will need to enter for example 'TV' or 'phone' to see results.";
 
     return (
         <div>
