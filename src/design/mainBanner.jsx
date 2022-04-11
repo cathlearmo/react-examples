e4d2afadccceab85098178ffb9809abde943450a
Catherine Learmonth <cathlearmo@gmail.com>
2022-04-11 20:10:59 +0100

    Add blue button for mobile and desktop

--- src/design/mainBanner.jsx
+++ src/design/mainBanner.jsx
@@ -7,13 +7,16 @@ export function MainBanner() {
 
     const isDesktop = useMediaQuery({ minWidth: 992 });
 
-    const titleClass = isDesktop ? 'main-banner-title-desktop' : 'main-banner-title-mobile';
+    const contentClass = isDesktop ? 'centered desktop' : 'centered mobile';
 
     return (
         <div className="main-banner">
-            <img src={mainBannerImage} alt="Snow" style={{width: "100%"}}/>
-            <div className="centered">
-                <label className={titleClass}>Crafting Spirits with Passion</label>
+            <img src={mainBannerImage}/>
+            <div className={contentClass}>
+                <label>Crafting Spirits with Passion</label>
+                <div>
+                    <button className="blue-button" type="button">VISIT OUR SHOP</button>
+                </div>
             </div>
         </div>
     );
