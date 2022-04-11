b4acd48b7b84e64674ee5e863e8c0ceb174aeb82
Catherine Learmonth <cathlearmo@gmail.com>
2022-04-11 22:48:37 +0100

    set product item para height and bold the price

--- src/design/productItem.jsx
+++ src/design/productItem.jsx
@@ -6,7 +6,7 @@ export function ProductItem({image, text, price}) {
     return (
         <div className={'product-item'}>
             <img src={image}/>
-            <p>{text}</p>
+            <p className="product-text">{text}</p>
             <p className="price">{price}</p>
             <div>
                 <button className="pink-button" type="button">VIEW DETAILS</button>
