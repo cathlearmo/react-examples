7b89289329ed03ff365001edd5ba997a8bab8e0d
Catherine Learmonth <cathlearmo@gmail.com>
2022-04-12 16:24:11 +0100

    add different mobile main image

--- src/design/mainBanner.jsx
+++ src/design/mainBanner.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import mainBannerImage from './designImages/mobile-main-banner-image.jpg';
+import mobileMainBannerImage from './designImages/mobile-main-banner-image.jpg';
 import { useMediaQuery } from 'react-responsive';
 import { Button } from './button.jsx';
 import './design.scss';
@@ -8,10 +9,11 @@ export function MainBanner() {
 
     const isDesktop = useMediaQuery({ minWidth: 992 });
     const contentClass = isDesktop ? 'centered desktop' : 'centered mobile';
+    const image = isDesktop ? mainBannerImage : mobileMainBannerImage;
 
     return (
         <div className="main-banner">
-            <img src={mainBannerImage}/>
+            <img src={image}/>
             <div className={contentClass}>
                 <label>Crafting Spirits with Passion</label>
                 <Button className="blue-button" text="VISIT OUR SHOP"/>
