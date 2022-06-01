afcb05588b9924384d748352cae410ca850d2220
Catherine Learmonth <cathlearmo@gmail.com>
2022-06-01 10:07:50 +0100

    add classname property to use tabs

--- src/customHooks/useTabs.jsx
+++ src/customHooks/useTabs.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './tabs.scss';
 
-export const useTabs = (tabArray = []) => {
+export const useTabs = (tabArray = [], className = "tab-list") => {
     const firstTab = tabArray.length ? tabArray[0] : null;
     const [currentTab, setCurrentTab] = React.useState(firstTab);
 
-    const tabs = <div className='tab-list'>
+    const tabs = <div className={className}>
         {tabArray.map((tab) => <div key={tab} className={tab === currentTab ? 'current' : null} onClick={() => setCurrentTab(tab)}>{tab}</div>)}
     </div>
 
