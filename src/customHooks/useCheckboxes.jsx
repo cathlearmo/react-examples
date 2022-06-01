b6645b67d598a860b26b1cbd9f8e7ea8544e2355
Catherine Learmonth <cathlearmo@gmail.com>
2022-06-01 10:06:14 +0100

    add checkbox state to return for use checkboxes

--- src/customHooks/useCheckboxes.jsx
+++ src/customHooks/useCheckboxes.jsx
@@ -1,21 +1,38 @@
-/* returns checkboxes with labels and also handles their state 
-    checkboxItems (array of objects contains id, isChecked boolean, label), 
-    className (class for the section that shows the checkboxes)
+import React from 'react';
+
+/* returns checkboxes with labels and the checkbox state (whether they are checked or not) 
+    checkboxItems (array of objects contains id, isChecked boolean, label)
 */
 
-export function useCheckboxes(checkBoxItems = []) {
+export function useCheckboxes(checkBoxItems = [], className = "checkbox-group") {
 
-    const boxes = <div className={'checkbox-group'}>
+    const [checkBoxState, setCheckboxes] = React.useState(checkBoxItems);
+
+    const updateOption = (option) => {
+        checkBoxState.forEach((item) => {
+            const value = item.id || item.label;
+            if (value === option.id || value === option.label) {
+                item.isChecked = !option.isChecked;
+            }
+        });
+        setCheckboxes(checkBoxState);
+    }
+
+    const boxes = <div className={className}>
         {
-            checkBoxItems.map((option) => <div key={option.id || option.label} onClick={() => !option.isChecked}>
+            checkBoxState.map((option) => <div key={option.id || option.label} onClick={() => updateOption(option)}>
                 <label>{option.label}
-                    <input type="checkbox" defaultChecked={option.isChecked} onChange={() => !option.isChecked}/>
+                    <input type="checkbox" defaultChecked={option.isChecked} onChange={() => updateOption(option)}/>
                 </label>
             </div>)
         }
     </div> 
 
+    //checkboxState is returned so the calling component can use the checked boxes
+    //boxes is the jsx code returned
+
     return {
-        boxes
+        boxes,
+        checkBoxState
     };
 }	
